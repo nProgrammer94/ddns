454a3b3d6c838d3934a2f8ee2aee12673a59833e
Hồ Thanh Nam <58923848+nShieldSolo@users.noreply.github.com>
2021-02-07 13:15:08 +0700

    init Project

---
+++ ddns/ddns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ddns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
